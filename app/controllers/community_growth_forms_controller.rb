113555afc5dfdd90aa03caf06ce1708c6d84c4b0
Allistair Catto <Allistair Catto>
2016-01-07 13:21:17 +0200

    WIL 05

--- app/controllers/community_growth_forms_controller.rb
+++ app/controllers/community_growth_forms_controller.rb
@@ -74,6 +74,6 @@ class CommunityGrowthFormsController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def community_growth_form_params
-      params.require(:community_growth_form).permit(:description, :count, :percentage_cover_mean, :occurance_mean, :slope, :intercept, :percentage_cover, :proportional_cover, :mean_canopy_diameter, :individuals_per_hectare, :std_deviation, :std_error, :has_strong_competitor, :has_normal_competitor, :has_weak_competitor,:report_community_id)
+      params.require(:community_growth_form).permit(:description, :count, :percentage_cover_mean, :occurance_mean, :slope, :intercept, :percentage_cover, :proportional_cover, :mean_canopy_diameter, :individuals_per_hectare, :std_deviation, :std_error, :has_strong_competitor, :has_normal_competitor, :has_weak_competitor,:report_community_id,:order)
     end
 end
